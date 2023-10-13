988cabd50d7faeb6548a6a8edeb71fd589a5275c
muthu <muthu@DESKTOP-988U431.home>
2023-10-13 10:02:31 -0400

    First Project

---
+++ SepBatch/src/SepBatchDemo/SepBatchStudent.java
@@ -0,0 +1,11 @@
+package SepBatchDemo;
+
+public class SepBatchStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First project");
+
+	}
+
+}
